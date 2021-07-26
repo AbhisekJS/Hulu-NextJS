4d6cc42d794e7fd356c63cd89a7ad33576005655
Abhisek-Khuntia <abhisek.khuntia16@gmail.com>
2021-07-26 22:41:25 +0530

    genres updated

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -4,7 +4,7 @@ export default function HeaderItem({title,Icon}){
         <div className=' flex flex-col items-center cursor-pointer group
         w-12 sm:w-20 hover:text-white ml-5'>
             <Icon className='h-8 mb-1 group-hover:animate-bounce'/>
-            <p className='opacity-0 group-hover:opacity-100 tracking-widest'>
+            <p className='opacity-0 group-hover:opacity-100 tracking-widest transition-all duration-100 ease-in-out'>
                 {title}
                 </p>
         </div>
